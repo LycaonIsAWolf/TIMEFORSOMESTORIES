64e44f47f0b69e88fd83efdb8e50a8a8cd719c39
lycaon <lycaon@tilde.town>
2017-06-05 00:58:19 +0000

    init

---
+++ TIMEFORSOMESTORIES.py
@@ -0,0 +1,28 @@
+from mastodon import Mastodon
+import json, random, threading, os
+
+min_delay = 600
+max_delay = 1800
+
+quotes = []
+with open('corpus.txt', 'r') as file:
+	quotes = file.read().split('\n')
+
+#get login info from secrets.json
+secrets = {}
+with open('secrets.json', 'r') as f:
+	secrets = json.loads(f.read())
+
+mastodon = Mastodon(client_id=secrets["id"], client_secret=secrets["secret"], access_token=secrets["access_token"], api_base_url="https://botsin.space")
+
+mastodon = Mastodon(client_id=secrets["id"], client_secret=secrets["secret"], access_token=secrets["access_token"], api_base_url="https://botsin.space")
+
+
+def make_post():
+	quote = random.choice(quotes)
+	print("posting {}".format(quote))
+	mastodon.status_post(quote, visibility="unlisted")
+
+	threading.Timer(random.randint(min_delay, max_delay), make_post).start()
+
+make_post()
---
+++ readme.md
@@ -0,0 +1,5 @@
+[TIME FOR SOME STORIES](https://web.archive.org/web/20161007080252/http://tilde.club/~maryr/TIMEFORSOMESTORIES.html)
+
+[A MASTODON ROBOT](https://botsin.space/@TIMEFORSOMESTORIES)
+
+[BY @LYCAON@WITCHES.TOWN](https://witches.town/@lycaon)
