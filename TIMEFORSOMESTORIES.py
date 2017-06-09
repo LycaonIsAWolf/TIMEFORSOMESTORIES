0709ae04bc83b24f24ce2f9b8612d50785f37cdd
lycaon <lycaon@tilde.town>
2017-06-09 20:49:42 +0000

    changed how quotes are chosen to get a bigger variety of quotes

--- TIMEFORSOMESTORIES.py
+++ TIMEFORSOMESTORIES.py
@@ -5,8 +5,11 @@ min_delay = 600
 max_delay = 1800
 
 quotes = []
+index = 0
 with open('corpus.txt', 'r') as file:
 	quotes = file.read().split('\n')
+
+random.shuffle(quotes)
 
 #get login info from secrets.json
 secrets = {}
@@ -16,10 +19,21 @@ with open('secrets.json', 'r') as f:
 mastodon = Mastodon(client_id=secrets["id"], client_secret=secrets["secret"], access_token=secrets["access_token"], api_base_url="https://botsin.space")
 
 def make_post():
-	quote = random.choice(quotes)
+	global index
+	if index >= len(quotes):
+		index = 0
+		random.shuffle(quotes)
+
+	quote = quotes[index]
+	index += 1
 	
 	while len(quote) > 500:
-		quote = random.choice(quotes)
+		print("skipping {} because it is too long".format(quote))
+		if index >= len(quotes):
+			index = 0
+			random.shuffle(quotes)
+		quote = quotes[index]
+		index += 1
 	
 	print("posting {}".format(quote))
 	mastodon.status_post(quote, visibility="unlisted")
